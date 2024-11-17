33d6d3ca1cbce15339006b4384088db8ba76debe
Anton Gladyshev <returnstrike@yandex.com>
2024-11-18 01:36:25 +0200

    fix current state so far

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # gt-ddl-checker
+
+curl -X POST -d '{"migration":"ALTER TABLE titles ADD INDEX idx_to_date(to_date)", "cluster":"employees"}' localhost:5555/check_migration
--- ddl_checker.py
+++ ddl_checker.py
@@ -3,17 +3,18 @@ import json
 import mysql.connector
 import configparser
 import re
+import math
 
 app = Flask(__name__)
 
 class DBConnect:
-    def __init__(self):
+    def __init__(self, cluster):
         self.con = mysql.connector.connect(
-             host        = myconfig["db"]["host"],
-             user        = myconfig["db"]["username"],
-             password    = myconfig["db"]["password"],
-             database    = myconfig["db"]["db"],
-             port        = myconfig["db"]["port"],
+             host        = myconfig[cluster]["host"],
+             user        = myconfig[cluster]["username"],
+             password    = myconfig[cluster]["password"],
+             database    = myconfig[cluster]["db"],
+             port        = myconfig[cluster]["port"],
              connection_timeout=86400,
              auth_plugin = 'mysql_native_password',
              autocommit  = True
@@ -59,16 +60,17 @@ def get_migration_type(migration: str)->tuple[str, int, bool]:
                 return "adding column at 0 pos requires table rebuild", 7, True
             else:
                 return "could be done safely", 3, False
+        return "not defined", -1, True
     elif "drop index" in migration:
         return "drop index", 5, True # TODO check this!
     elif "rename table" in migration:
-        return "rename table", 5
+        return "rename table", 5, True
     elif "truncate table" in migration:
-        return "truncate table", 8
+        return "truncate table", 8, True
     elif any(admin_func in migration for admin_func in admin_functions):
-        return "admin func", 10
+        return "admin func", 10, True
     else:
-        return "not defined", -1
+        return "not defined", -1, True
 
 
 def get_affected_db_table(migration: str)->tuple[str, str]:
@@ -96,28 +98,64 @@ def get_affected_db_table(migration: str)->tuple[str, str]:
         return dbname, tablename
 
 def check_size(cluster, db, aff_table)->tuple[float, float]:
-    if cluster not in myconfig.keys:
+    #print("we are inside check_size")
+    #print(cluster, db, aff_table)
+    if cluster not in myconfig.keys():
         return -1, -1
-    return 0, 0
+    db_con = DBConnect(cluster)
+    if db is None:
+        db_con.cur.execute("""SELECT table_rows, 
+                              data_length,
+                              index_length
+                              FROM information_schema.tables
+                              WHERE table_name = %(table_name)s""",
+                              {"table_name":   aff_table})
+    else:
+        db_con.cur.execute("""SELECT table_rows, 
+                              data_length,
+                              index_length
+                              FROM information_schema.tables
+                              WHERE table_name = %(table_name)s
+                              AND table_schema= %(table_schema)s;""",
+                              {"table_name":   aff_table,
+                               "table_schema": db})
+    myresult = db_con.cur.fetchall()
+    db_con.close()
+    if len(myresult)>0:
+        data_size_b  = myresult[0][1]
+        index_size_b = myresult[0][2]
+        size_in_mb = math.ceil(data_size_b+index_size_b/1024/1024)
+        rows_number  = myresult[0][0]
+        return rows_number, size_in_mb
+    return -1, -1
 
-@app.route('/check_migration')
+@app.route('/check_migration', methods=['POST','OPTIONS'])
 def check_migration():
     reqdata            = request.get_data().decode()
     reqobj             = json.loads(reqdata)
     migration: str     = reqobj.get("migration",None).lower()
     cluster: str       = reqobj.get("cluster",None).lower()
     
+    #print(f'migration for assessment: {migration}')
+    #print(f'cluster name is {cluster}')
     migration_type, risk, size_dep  = get_migration_type(migration)
 
     if size_dep:
         db, aff_table = get_affected_db_table(migration)
     if aff_table is not None:
-        size, length = check_size(cluster, db, aff_table)
+        rows_no, size_mb = check_size(cluster, db, aff_table)
 
-    return migration_type, risk, None
+    #return {migration_type, risk, None}, 200, 
+    return {"cont":migration_type, "risk":risk}, 200
 
 
 if __name__ == '__main__':
     myconfig = {}
+    cheaders_p = {
+        "Content-Type":"application/json; charset=utf-8",
+        "Access-Control-Allow-Origin":"*",
+        "Access-Control-Allow-Methods":["POST","OPTIONS"],
+        "Access-Control-Expose-Headers":"*"
+    }
     read_ini("config.ini")
     app.run(debug=True, port=5555)
